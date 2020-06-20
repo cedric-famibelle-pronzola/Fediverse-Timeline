f015f95dc485c7fba799fe5e90d5dab0fb66ca52
Cedric FAMIBELLE-PRONZOLA <c.pronzola@live.fr>
2020-06-20 12:19:27 +0200

    Use curl instead of get contents

--- contents.php
+++ contents.php
@@ -1,10 +1,15 @@
 <?php
 
 function get(){
-    $apiUrl = 'https://mamot.fr/api/v1/timelines/public';
-    $json = file_get_contents($apiUrl);
-    $result = json_decode($json);
-    return $result;
+  $url = 'https://mamot.fr/api/v1/timelines/public?local=true';
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, [
+      'Content-Type: application/json'
+    ]);
+  $response = json_decode(curl_exec($curl));
+  curl_close($curl);
+  return $response;
 }
 
 $pages = get();
